b96deba21f2eba147221b320c6038c9df54ba518
Tyler Nguyen <tylern41653@gmail.com>
2023-06-03 00:48:50 -0400

    ttt

--- client_folder/client/src/scenes/widgets/PostsWidget.jsx
+++ client_folder/client/src/scenes/widgets/PostsWidget.jsx
@@ -9,7 +9,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
   const token = useSelector((state) => state.token);
 
   const getPosts = async () => {
-    const response = await fetch("/posts", {
+    const response = await fetch("https://zephyr-app.herokuapp.com//posts", {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
