4472ea7da641a1d501aa2b574cb4f8bc09969cd2
Tyler Nguyen <tylern41653@gmail.com>
2023-06-03 01:34:04 -0400

    ttt

--- client_folder/client/src/components/Friend.jsx
+++ client_folder/client/src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `/users/${_id}/${friendId}`,
+      `http://localhost:3001/users/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
--- client_folder/client/src/scenes/widgets/PostsWidget.jsx
+++ client_folder/client/src/scenes/widgets/PostsWidget.jsx
@@ -19,7 +19,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
 
   const getUserPosts = async () => {
     const response = await fetch(
-      `/posts/${userId}/posts`,
+      `http://localhost:3001/users/posts/${userId}/posts`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
